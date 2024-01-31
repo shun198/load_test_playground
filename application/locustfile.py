d377c9d4f332b1c4ebf5952783238e3b79768d2e
s.hirose <shunichi_hirose@pj100.biz>
2024-01-31 09:17:16 +0900

    👍 add container name

--- application/locustfile.py
+++ application/locustfile.py
@@ -5,6 +5,5 @@ class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+    def health(self):
+        self.client.get("http://127.0.0.1:8000/api/health/")
