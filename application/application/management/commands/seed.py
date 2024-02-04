2c681dda08c1f3d033e74ad341b44fa092a82e37
shun198 <shunhiroseluvmri@gmail.com>
2024-02-04 13:46:38 +0900

    👍 create or update group

--- application/application/management/commands/seed.py
+++ application/application/management/commands/seed.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
 
 
@@ -5,6 +6,11 @@ class Command(BaseCommand):
     help = "Seeds the database with initial data"
 
     def add_arguments(self, parser):
+        """システムユーザの数を指定するオプション
+
+        Args:
+            parser : オプションを追加するためのparser
+        """
         parser.add_argument(
             "--users",
             type=int,
@@ -12,8 +18,21 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        """システムユーザを作成するコマンド"""
+        self._create_or_update_group()
         users = options["users"]
         if not users:
-            print("no users")
             users = 10
-            print(users)
+        for i in range(1, users + 1):
+            print(f"{i}:作成開始")
+
+    def _create_or_update_group(self):
+        """システムユーザの権限グループを作成または更新するメソッド"""
+        Group.objects.update_or_create(
+            id=1,
+            name="管理者",
+        )
+        Group.objects.update_or_create(
+            id=2,
+            name="一般",
+        )
